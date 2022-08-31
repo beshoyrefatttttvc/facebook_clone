fa4827f4781eff110062063926ce5bf26bdf70fd
beshoyrefatttttvc <poshrefat@gmail.com>
2022-08-31 20:39:02 +0200

    facebook level one

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FacebookApp(),
+    );
+  }
+}
+
+class FacebookApp extends StatelessWidget {
+  const FacebookApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "facebook",
+          style: TextStyle(
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+          color: Colors.blue,
+          iconSize: 30,
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+            iconSize: 30,
+            color: Colors.blue,
+          ),
+          IconButton(
+            icon: const Icon(Icons.messenger_rounded),
+            onPressed: () {},
+            iconSize: 30,
+            color: Colors.blue,
+          ),
+        ],
+        backgroundColor: Colors.white,
+        elevation: 5,
+      ),
+    );
+  }
+}
+
